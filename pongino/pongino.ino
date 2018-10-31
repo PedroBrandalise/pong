2013bbc5e8ddb31251e0faf6dd43855f2f476dbc
Jesuan <jesuanperes@gmail.com>
2018-10-31 10:35:42 -0300

    pong display 3

--- pongino/pongino.ino
+++ pongino/pongino.ino
@@ -1,31 +1,60 @@
-#define TAMMX 20
-#define TAMMY 33
+//display libs:
+ 
+ #include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+ #include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
+/*****************************************************************/
+
+int TAMMX;
+int TAMMY;
 #define ESQUERDA -1
 #define DIREITA 1
 #define CIMA 1
 #define BAIXO -1
 
 
+
 //definir os pinos
 #define pinoBotao1 D8
 #define pinoBotao2 D7
+
+//display
+SSD1306Wire  display(0x3c, D3, D5);
+#define BOLA_TAM 1
+#define R_TAM 8
 
 void inicializacao();
 void recebeComandos();
 void logica();
 void escreveTela();
+void desenhaTela();
 
-char matrix[TAMMX][TAMMY];
-int posicaoX= TAMMX-3, posicaoY= TAMMY/2;
-int posicaoRaqueteX= TAMMX-2, posicaoRaqueteY = TAMMY/2;
+//char matrix[TAMMX][TAMMY];
+
 int dirx = ESQUERDA;
 int diry = CIMA;
+int posicaoX;
+int posicaoY;
+int posicaoRaqueteX;
+int posicaoRaqueteY;
+
 
 void setup() {
-  inicializacao();
+  TAMMX = display.getWidth(); 
+  TAMMY = display.getHeight();
+  posicaoX= TAMMX-3, posicaoY= TAMMY/2;
+  posicaoRaqueteX= R_TAM, posicaoRaqueteY = 0;
+  
+  //inicializacao();
   Serial.begin(57600);
   pinMode(pinoBotao1, INPUT );
   pinMode(pinoBotao2, INPUT );
+
+  //display config
+  display.init();
+
+  // display.flipScreenVertically();
+
+  display.setContrast(255);
 
 }
 
@@ -34,9 +63,11 @@ void loop() {
   //delay(50);
   recebeComandos();
   logica();
-  escreveTela();
-
+  //escreveTela();
+  desenhaTela();
+  delay(20);
 }
+/*
 void inicializacao(){
   for (int i = 0; i < TAMMX; ++i)
    {
@@ -52,24 +83,25 @@ void inicializacao(){
    } 
    //matrix[posicaoX][posicaoY]= 'o';
 }
+*/
 
 void recebeComandos(){
    char controle= getchar();
    
    if(digitalRead(pinoBotao1)== HIGH ) {
-    if(posicaoRaqueteY == TAMMY-2){
+    if(posicaoRaqueteX <= TAMMX-2){
       
     }else{
-      posicaoRaqueteY++;
+      posicaoRaqueteX+=2;
     }
     
     Serial.println(">");
     }
    if(digitalRead(pinoBotao2)== HIGH ) {
-    if(posicaoRaqueteY == 1){
+    if(posicaoRaqueteX >= 1){
       
     }else{
-      posicaoRaqueteY--;
+      posicaoRaqueteX-=2;
     }
     Serial.println("<");
    }
@@ -116,7 +148,7 @@ void logica(){
 
 }
 
-
+/*
 void escreveTela(){
   
   for (int i = 0; i < TAMMX; ++i)
@@ -150,8 +182,20 @@ void escreveTela(){
 
    //system("clear");
 
+  
 
+}
+*/
 
+void desenhaTela(){
+  display.clear();
+  //desenha a bolinha
+  display.drawCircle(posicaoX, posicaoY, BOLA_TAM);
+  //desenha duas requeti
+  display.drawRect(posicaoRaqueteX -R_TAM, posicaoRaqueteY, 2 * R_TAM, 2);
+    
+  //display.drawRect(0, 0, 16, 2);
+  display.display();
 }
 
 
